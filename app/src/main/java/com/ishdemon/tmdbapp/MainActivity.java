cbe9fc4bbe4226eefa2428811ffddad2e6ca5509
Ishank Singh <ishdemon10@gmail.com>
2019-04-08 22:26:09 +0530

    initial commit - New project

---
+++ app/src/main/java/com/ishdemon/tmdbapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ishdemon.tmdbapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
